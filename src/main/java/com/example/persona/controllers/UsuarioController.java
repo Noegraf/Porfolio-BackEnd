e5b4aa456b1414cf158731a63a487c4d95bdc7aa
Noe Valle <nouvalle.graf@gmail.com>
2022-09-17 01:36:46 -0300

    make it better

--- src/main/java/com/example/persona/controllers/UsuarioController.java
+++ src/main/java/com/example/persona/controllers/UsuarioController.java
@@ -12,21 +12,22 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "/api/usuario" )
-public class UsuarioController {
 
+
+public class UsuarioController {
 private final UsuarioService usuarioService;
 
     public UsuarioController(UsuarioService usuarioService) { this.usuarioService = usuarioService; }
 
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> obtenerUsuario(@PathVariable("id") Long id) {
-        Usuario usuario = (Usuario) usuarioService.buscarUsuarioPorId(id);
+        Usuario usuario =  usuarioService.buscarUsuarioPorId(id);
         return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
 
     @PutMapping
     public ResponseEntity<Usuario> editarUsuario(@RequestBody Usuario usuario) {
-        Usuario result = (Usuario) usuarioService.editarUsuario(usuario);
+        Usuario updateUsuario =  usuarioService.editarUsuario(usuario);
 
         return new ResponseEntity<>(usuario, HttpStatus.OK);
     }
--- src/main/java/com/example/persona/security/AuthApi.java
+++ src/main/java/com/example/persona/security/AuthApi.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping
 public class AuthApi {
     @Autowired
